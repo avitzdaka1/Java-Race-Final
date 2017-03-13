bd69932e4f0b1c8b0a21b87389a346a175a890fa
Omer Yaari <omermyaari@gmail.com>
2017-03-13 17:30:04 +0200

    table update

--- CarRaceMVC_Project/src/Server/RACE_DB.sql
+++ CarRaceMVC_Project/src/Server/RACE_DB.sql
@@ -14,6 +14,6 @@ drop table if exists GamblerCarRace;
 create table Gambler (id integer not null unique, name varchar(10) not null unique, password varchar(10) not null, balance integer, constraint pkId primary key (id));
 create table Race (number integer not null unique, date raceDate, state integer, totalBets integer, constraint pkNumber primary key (number));
 create table Car (name varchar(10) not null unique, make varchar(10) not null, size varchar(10) not null, color varchar(10) not null, type varchar(10) not null);
-create table RaceResult(raceNumber integer not null, gamblerId integer not null, revenue integer, constraint fkRaceNumber foreign key (raceNumber) referencees Race(number), constraint fkGamblerId foreign key (gamblerId) references Gambler(id));
-creare table GamblerCarRace(gamblerId integer not null, raceNumber integer not null, carName varchar(10) not null, bet integer, constraint fkGamblerId foreign key (gamblerId) referencees Gambler(id), constraint fkRaceNumber foreign key (raceNumber) referencees Race(number), constraint fkCarName foreign key (carName) references Car(name));
+create table RaceResult(raceNumber integer not null, gamblerId integer not null, revenue integer, position integer, constraint fkRaceNumber foreign key (raceNumber) referencees Race(number), constraint fkGamblerId foreign key (gamblerId) references Gambler(id));
+create table GamblerCarRace(gamblerId integer not null, raceNumber integer not null, carName varchar(10) not null, bet integer, constraint fkGamblerId foreign key (gamblerId) referencees Gambler(id), constraint fkRaceNumber foreign key (raceNumber) referencees Race(number), constraint fkCarName foreign key (carName) references Car(name));
 
